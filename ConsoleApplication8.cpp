ae32ac756b990a45f5c1228308ccfda52a5d5db3
mayar <Mayar.20375806@compit.aun.edu.eg>
2021-10-20 13:59:22 +0200

    first code

---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication8.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, s, sum
+		cout << "enter yuor nom / n";
+	cin >> m >> s;
+	sum = m + s;
+	cout << "sum";
+
+}
+    return 0;
+}
+
